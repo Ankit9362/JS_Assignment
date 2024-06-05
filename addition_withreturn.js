3a7fa3a22093b6cbb9df0a4be6c02d66ac692ad7
Ankit9362 <166363077+Ankit9362@users.noreply.github.com>
2024-06-05 20:50:23 +0530

    save

---
+++ addition_withreturn.js
@@ -0,0 +1,11 @@
+function addition(no)
+{
+    var add = 0;
+    for(i=0;i<=no;i++)
+        {
+            add = add + i;
+        }
+        return add;
+}
+var total = addition(4);
+console.log(total);
